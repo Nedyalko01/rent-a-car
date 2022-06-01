18e61ff80bff33ef2029a98e102cb855b1ad89e5
Nedyalko <102542299+nedK01@users.noreply.github.com>
2022-06-01 17:19:43 +0300

    update

--- src/main/java/com/rentacar/rentacar/controller/CustomerController.java
+++ src/main/java/com/rentacar/rentacar/controller/CustomerController.java
@@ -3,11 +3,9 @@ package com.rentacar.rentacar.controller;
 
 import com.rentacar.rentacar.converter.CustomerConverter;
 import com.rentacar.rentacar.dto.CustomerRequest;
-import com.rentacar.rentacar.dto.CustomerResponse;
 import com.rentacar.rentacar.entity.Customer;
 import com.rentacar.rentacar.service.CustomerService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +19,11 @@ public class CustomerController {
     private final CustomerConverter customerConverter;
 
     @PostMapping
-    public ResponseEntity<CustomerResponse> save (@RequestBody final CustomerRequest customerRequest) {
+    public ResponseEntity<HttpStatus> save (@RequestBody final CustomerRequest customerRequest) {
         final Customer customer = customerConverter.map(customerRequest);
         final Customer saved = customerService.save(customer);
 
-        return null;
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
